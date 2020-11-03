2e21d849d710c86e968d57706cfbed1b4043bb85
Ahadian Akbar <rabkawork@gmail.com>
2020-11-03 10:45:49 +0700

    implement adapter to load data in mainactivity

--- app/src/main/java/com/rab/adapter_cardview/MainActivity.kt
+++ app/src/main/java/com/rab/adapter_cardview/MainActivity.kt
@@ -2,10 +2,29 @@ package com.rab.adapter_cardview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.rab.adapter_cardview.adapter.ProdukAdapter
+import com.rab.adapter_cardview.models.Produk
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.daftar_produk.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var products : ArrayList<Produk> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvProduct.layoutManager = GridLayoutManager(this, 2)
+
+        var price = 2000
+        for (i in 1..10) {
+            price += i * 10
+            val product = Produk("Nama Produk $i", price)
+            products.add(product)
+        }
+
+        rvProduct.adapter = ProdukAdapter(products)
     }
 }
---
+++ app/src/main/java/com/rab/adapter_cardview/adapter/ProdukAdapter.kt
@@ -0,0 +1,45 @@
+package com.rab.adapter_cardview.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.rab.adapter_cardview.R
+import com.rab.adapter_cardview.models.Produk
+import kotlinx.android.synthetic.main.daftar_produk.view.*
+
+class ProdukAdapter(val products: ArrayList<Produk>) : RecyclerView.Adapter<ProdukAdapter.ViewHolder>() {
+    override fun getItemCount(): Int = products.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        var view : View = LayoutInflater.from(parent.context).inflate(R.layout.daftar_produk, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindData(products.get(position))
+    }
+
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        private var view : View = itemView
+        private lateinit var dataProduk : Produk
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(p0: View) {
+            Toast.makeText(view.context, "${dataProduk.nama} Diklik", Toast.LENGTH_SHORT).show()
+        }
+
+        fun bindData(product: Produk) {
+            this.dataProduk = product
+            //view.productImage.setImageResource(R.drawable.product)
+            view.productName.setText(product.nama)
+            view.productPrice.setText(product.harga.toString())
+        }
+    }
+}
